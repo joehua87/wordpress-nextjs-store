8e4527aee9b20c9f3e6046ae3e46d4d40eeca16b
Joe Hua <joehua87@gmail.com>
2021-09-06 01:08:40 +0700

    improve images; do not auto clear cache

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPreact = require('next-plugin-preact')
 /** @type {import('next').NextConfig} */
 module.exports = withPreact({
   reactStrictMode: true,
+  cleanDistDir: false,
   images: {
     domains: ['docker', 'admin.wordpress-demo.dew.vn'],
   },
